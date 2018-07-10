06f753b2ded1b2536b5c8b5b48477f7be30bd97e
Guest User <Guest@epicodus-25.local>
2018-07-10 10:12:37 -0700

    Add Jest testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ function Header(){
   return (
     <div>
       <h1>Help Queue</h1>
-      <button className="btn btn-danger" type="submit">Sbu</button>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   )
